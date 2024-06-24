9a92f02c79354dad1c65f7c5027c6530dea29a90
Ronald Camargo <ronaldcamargodev@gmail.com>
2024-06-24 10:42:07 -0400

    criando tela sobre mim

--- src/page/about/about.tsx
+++ src/page/about/about.tsx
@@ -1,7 +1,23 @@
+import { FaGraduationCap, FaHouseDamage } from "react-icons/fa";
+import { MdDriveFileRenameOutline } from "react-icons/md";
 export default function About() {
     return (
-        <div className="h-screen bg-slate-800 flex p-6">
-            <h1></h1>
+        <div className="h-screen bg-slate-800 flex flex-col p-6">
+            <h1 className="w-full text-center text-[50px] font-semibold text-gray-300">Quem sou eu?</h1>
+            <div>
+                <div>
+                    <h2 className="text-2xl text-gray-300 flex items-center"><MdDriveFileRenameOutline className="mr-[5px] text-[#00BFFF]" /> Eu</h2>
+                    <p className="text-gray-300">Me chamo Ronald Camargo tenho 23 anos.</p>
+                </div>
+                <div>
+                    <h2 className="text-2xl text-gray-300 flex items-center"><FaGraduationCap className="mr-[5px] text-[#00BFFF]" /> Formação</h2>
+                    <p className="text-gray-300">Sou formado em Engenharia da Computação - Bacharelado pela Universidade Federal de Mato Grosso (2018-2023).</p>
+                </div>
+                <div>
+                    <h2 className="text-2xl text-gray-300 flex items-center"><FaHouseDamage className="mr-[5px] text-[#00BFFF]" /> Endereço</h2>
+                    <p className="text-gray-300">Resido em Planalto da Serra - MT.</p>
+                </div>
+            </div>
         </div>
     )
 }
