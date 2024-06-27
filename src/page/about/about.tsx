63e864414f1d67a77156183c58cb7c6aa9d65081
Ronald Camargo <ronaldcamargodev@gmail.com>
2024-06-26 21:20:53 -0400

    finalizando sobre

--- src/page/about/about.tsx
+++ src/page/about/about.tsx
@@ -1,13 +1,13 @@
-import { Button } from "@/components/ui/button";
-import { FaGraduationCap, FaHouseDamage } from "react-icons/fa";
-import { MdDriveFileRenameOutline } from "react-icons/md";
 import { IoMdCodeDownload } from "react-icons/io";
+import { MdDriveFileRenameOutline } from "react-icons/md";
+import { FaGraduationCap, FaHouseDamage } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
 import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
 
 export default function About() {
     return (
         <div className="min-h-screen bg-slate-800 flex flex-col p-6">
-            <div className="mb-12">
+            <div>
                 <div className="flex justify-between items-center mb-6">
                     <h1 className="w-full text-center text-[50px] font-semibold text-gray-300">Quem sou eu?</h1>
                     <Button className="w-[200px] hover:bg-[#00BFFF] hover:text-slate-900 flex gap-[5px] font-bold">
@@ -21,61 +21,93 @@ export default function About() {
                         </Avatar>
                     </div>
                     <div className="flex flex-col gap-5 w-full lg:w-1/2">
-                        <div className="border-b-[1px] border-[#00BFFF] p-4">
-                            <h2 className="text-2xl text-gray-300 flex items-center pb-2">
-                                <MdDriveFileRenameOutline className="mr-[5px] text-[#00BFFF]" /> Eu
-                            </h2>
-                            <p className="text-gray-300 text-xl">
-                                Me chamo Ronald Camargo, tenho 23 anos.
-                            </p>
-                        </div>
-                        <div className="border-b-[1px] border-[#00BFFF] p-4">
-                            <h2 className="text-2xl text-gray-300 flex items-center pb-2">
-                                <FaGraduationCap className="mr-[5px] text-[#00BFFF]" /> Formação
-                            </h2>
-                            <p className="text-gray-300 text-xl">
-                                Sou formado em Engenharia da Computação - Bacharelado pela Universidade Federal de Mato Grosso (2018-2023).
-                            </p>
-                        </div>
-                        <div className="border-b-[1px] border-[#00BFFF] p-4">
-                            <h2 className="text-2xl text-gray-300 flex items-center pb-2">
-                                <FaHouseDamage className="mr-[5px] text-[#00BFFF]" /> Endereço
-                            </h2>
-                            <p className="text-gray-300 text-xl">
-                                Resido em Planalto da Serra - MT.
-                            </p>
-                        </div>
+                        {aboutInfo.map((info, index) => (
+                            <div key={index} className="border-b-[1px] border-[#00BFFF] p-4">
+                                <h2 className="text-2xl text-gray-300 flex items-center pb-2">
+                                    <info.icon className="mr-[5px] text-[#00BFFF]" /> {info.title}
+                                </h2>
+                                <p className="text-gray-300 text-xl">{info.description}</p>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
-
             <div>
-                <div className="flex justify-center mb-6">
+                <div className="flex justify-center mb-6 mt-6">
                     <h1 className="text-center text-[50px] font-semibold text-gray-300">Experiências</h1>
                 </div>
-                <div className="flex flex-col lg:flex-row justify-between">
-                    <div className="flex flex-col gap-5 w-full lg:w-1/2">
-                        <div className="border-[1px] rounded-[10px] border-[#00BFFF] p-4">
+                <div className="flex flex-wrap justify-between gap-5">
+                    {experiences.map((experience, index) => (
+                        <div key={index} className="border-[1px] rounded-[10px] border-[#00BFFF] p-4 flex-1 min-w-[300px]">
                             <h2 className="text-2xl text-gray-300 flex items-center pb-2 border-b-[1px] border-[#00BFFF]">
-                                <MdDriveFileRenameOutline className="mr-[5px] text-[#00BFFF]" /> BSN SOLUTIONS
+                                <MdDriveFileRenameOutline className="mr-[5px] text-[#00BFFF]" /> {experience.company}
                             </h2>
-                            <p className="text-gray-300 text-xl mt-2">
-                                Desenvolvedor Full Stack Júnior (2022 - Atual)
+                            <p className="flex flex-col text-gray-300 text-xl mt-2">
+                                {experience.role}
+                                <span className="text-slate-400 text-lg font-semibold"> ({experience.period})</span>
                             </p>
                             <h3 className="text-xl text-[#00BFFF] mt-2">Atividades:</h3>
                             <ul className="list-disc list-inside">
-                                <li className="text-gray-300 text-xl">Desenvolvimento de aplicações web.</li>
-                                <li className="text-gray-300 text-xl">Manutenção de aplicações web.</li>
-                                <li className="text-gray-300 text-xl">Desenvolvimento de APIs.</li>
-                                <li className="text-gray-300 text-xl">Desenvolvimento de banco de dados SQL e NoSQL.</li>
-                                <li className="text-gray-300 text-xl">Desenvolvimento em arquitetura MVC.</li>
+                                {experience.activities.map((activity, idx) => (
+                                    <li key={idx} className="text-gray-300 text-xl">{activity}</li>
+                                ))}
                             </ul>
-                            <h3 className="text-xl text-[#00BFFF] mt-2">Stacks:</h3>
-                            <p className="text-gray-300 text-xl">HTML, CSS, JavaScript, TypeScript, React, Node.js, SQL, MongoDB, MySQL, Git, GitHub</p>
+                            <h3 className="text-xl text-[#00BFFF] mt-2 mb-[10px]">Stacks:</h3>
+                            <ul className="flex flex-wrap gap-2">
+                                {experience.stacks.map((stack, idx) => (
+                                    <li key={idx} className="border-[2px] border-[#00BFFF] bg-slate-950 rounded-[8px] px-[15px] py-[3px] text-gray-300 font-bold">{stack}</li>
+                                ))}
+                            </ul>
                         </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>
     );
 }
+
+const aboutInfo = [
+    {
+        title: "Eu",
+        description: "Me chamo Ronald Camargo, tenho 23 anos.",
+        icon: MdDriveFileRenameOutline,
+    },
+    {
+        title: "Formação",
+        description: "Sou formado em Engenharia da Computação - Bacharelado pela Universidade Federal de Mato Grosso (2018-2023).",
+        icon: FaGraduationCap,
+    },
+    {
+        title: "Endereço",
+        description: "Resido em Planalto da Serra - MT.",
+        icon: FaHouseDamage,
+    },
+];
+
+const experiences = [
+    {
+        company: "BSN SOLUTIONS",
+        role: "Desenvolvedor Full Stack Júnior",
+        period: "2022 - 2023",
+        activities: [
+            "Desenvolvimento de aplicações web.",
+            "Manutenção de aplicações web.",
+            "Desenvolvimento de banco de dados NoSQL.",
+            "Desenvolvimento em arquitetura MVC.",
+        ],
+        stacks: ["HTML", "CSS", "JavaScript", "TypeScript", "React", "NodeJS", "NoSQL", "MongoDB", "Git", "GitHub", "AWS S3"],
+    },
+    {
+        company: "ATM SOLUÇÕES EM SERVIÇOS",
+        role: "Desenvolvedor Full Stack Júnior",
+        period: "2023 - Atual",
+        activities: [
+            "Desenvolvimento de aplicações web.",
+            "Manutenção de aplicações web.",
+            "Desenvolvimento de APIs RestFul.",
+            "Desenvolvimento de banco de dados SQL.",
+            "Desenvolvimento em arquitetura MVC.",
+        ],
+        stacks: ["JavaScript", "TypeScript", "React", "NodeJS", "SQL", "SQL SERVER", "Git", "GitHub", "Azure"],
+    },
+];
