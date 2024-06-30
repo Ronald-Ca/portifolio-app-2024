d27a60a3378be18aaf48297414084f46e524b032
Ronald Camargo <ronaldcamargodev@gmail.com>
2024-06-29 21:05:05 -0400

    finalizando tela de projetos

--- src/page/projects/projects.tsx
+++ src/page/projects/projects.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button';
 import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { DialogHeader } from '@/components/ui/dialog';
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from '@radix-ui/react-dialog';
+import { useState } from 'react';
 import { IoIosClose } from "react-icons/io";
 
 interface Project {
@@ -10,6 +11,8 @@ interface Project {
     stars: number;
     previewImage: string;
     description: string;
+    link: string;
+    stacks?: string[];
 }
 
 const projects: Project[] = [
@@ -19,8 +22,23 @@ const projects: Project[] = [
         stars: 3,
         previewImage: "src/assets/nlw-explorer.png",
         description: "Este é um projeto, bem projetado, cheio de projetos, improjetável.",
+        link: "https://ronald-ca.github.io/nlw-site-explorer/",
+        stacks: ["HTML", "CSS"],
     },
 ];
+
+const stackColors: { [key: string]: string } = {
+    HTML: "bg-[#FF4500]",
+    CSS: "bg-[#00BFFF]",
+    JavaScript: "bg-[#FFFF00]",
+    TypeScript: "bg-[#007ACC]",
+    React: "bg-[#68A063]",
+    NodeJS: "bg-[#4DB33D]",
+    Python: "bg-[#FFD700]",
+    Django: "bg-[#0DB7ED]",
+    Git: "bg-[#F05032]",
+    GitHub: "bg-[#171515]",
+};
 
 export default function Projects() {
     return (
@@ -42,8 +60,10 @@ interface ProjectCardProps {
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
-        <Dialog>
+        <Dialog open={isOpen} onOpenChange={setIsOpen}>
             <DialogTrigger asChild>
                 <Card className="bg-slate-950 w-[400px] border-[2px] rounded-[10px] border-[#00BFFF] p-[20px] text-center cursor-pointer transform transition-transform duration-300 hover:scale-105">
                     <CardHeader>
@@ -53,33 +73,36 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
                     </CardHeader>
                 </Card>
             </DialogTrigger>
-            <DialogContent className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 ">
-                <div className="bg-slate-900 rounded-lg p-6 max-w-lg mx-auto relative border-[2px] border-[#00BFFF]">
+            <DialogContent className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50" onClick={() => setIsOpen(false)}>
+                <div className="bg-slate-900 rounded-lg p-6 max-w-[800px] mx-auto relative border-[2px] border-[#00BFFF]" onClick={(e) => e.stopPropagation()}>
                     <DialogClose asChild>
-                        <Button className='absolute top-4 right-4'>
+                        <Button className='absolute top-0 right-0' onClick={() => setIsOpen(false)}>
                             <IoIosClose size={30} className="text-[#00BFFF]" />
                         </Button>
                     </DialogClose>
                     <DialogHeader>
-                        <DialogTitle>{project.name}</DialogTitle>
-                        <DialogDescription>{project.description}</DialogDescription>
+                        <DialogTitle className='text-gray-100 text-center font-semibold  text-[30px]'>{project.name}</DialogTitle>
+                        <video className='w-[100%] h-[350px] rounded-[10px] border-[2px] border-[#00BFFF]' src="src/assets/nlw.mp4" autoPlay controls></video>
+                        <DialogTitle className='text-gray-100 font-semibold  text-[20px]'>Descrição:</DialogTitle>
+                        <DialogDescription className='text-gray-100 text-[15px] rounded-[10px] border-[2px] border-[#00BFFF] p-[8px]'>{project.description}</DialogDescription>
+                        <DialogTitle className='text-gray-100 font-semibold  text-[20px]'>Link:</DialogTitle>
+                        <DialogDescription className='text-gray-100 text-[15px] rounded-[10px] border-[2px] border-[#00BFFF] p-[8px]'>
+                            <a href="https://ronald-ca.github.io/nlw-site-explorer/" target="_blank" rel="noreferrer" className='transition-colors duration-[3000ms] hover:text-[#00BFFF] underline decoration-solid' >
+                                {project.link}
+                            </a>
+                        </DialogDescription>
+                        <DialogTitle className='text-gray-100 font-semibold  text-[20px]'>Stacks:</DialogTitle>
+                        <DialogDescription className='text-gray-100 text-[15px] rounded-[10px] border-[2px] border-[#00BFFF] p-[8px]'>
+                            {project.stacks?.map((stack, index) => (
+                                <span key={index} className={`mr-2 text-slate-900 ${stackColors[stack]} rounded-[4px] pb-[2px] pl-[5px] pr-[5px] font-semibold`}>
+                                    {stack}
+                                </span>
+                            ))}
+                        </DialogDescription>
                     </DialogHeader>
-                    {/* <ProjectDetails project={project} /> */}
                 </div>
             </DialogContent>
         </Dialog>
     )
 }
 
-// interface ProjectDetailsProps {
-//     project: Project;
-// }
-
-// const ProjectDetails: React.FC<ProjectDetailsProps> = ({ project }) => {
-//     return (
-//         <div className="p-4">
-//             <div className="flex justify-center">
-//             </div>
-//         </div>
-//     )
-// }
