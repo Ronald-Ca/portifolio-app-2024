c01b8c905413ff2e7e57d6d66521101897a4338c
Ronald Camargo <ronaldcamargodev@gmail.com>
2024-07-03 23:53:12 -0400

    fazendo alterações na edição da tela de inicio

--- src/page/config/config-home.tsx
+++ src/page/config/config-home.tsx
@@ -1,11 +1,44 @@
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { useRef, useState } from "react";
+import { FaCamera } from "react-icons/fa";
 
 export default function ConfigHome() {
+    const [imagePreview, setImagePreview] = useState('https://avatars.githubusercontent.com/u/104284345?v=4');
+    const fileInputRef = useRef(null)
+
+    const handleImageChange = (event: any) => {
+        const file = event.target.files[0];
+        if (file) {
+            const imageURL = URL.createObjectURL(file);
+            setImagePreview(imageURL);
+        }
+    }
+
+    const handleCameraClick = () => {
+        fileInputRef.current.click()
+    }
+
     return (
         <div className="flex justify-center gap-[50px]">
-            <div>
-                <Input type="file" className="cursor-pointer" />
+            <div className="flex flex-col items-center gap-[10px] relative">
+                {imagePreview && (
+                    <img
+                        src={imagePreview}
+                        alt="Preview"
+                        className="w-[250px] h-[250px] object-cover rounded-full"
+                    />
+                )}
+                <FaCamera
+                    className="text-[#00BFFF] text-[35px] cursor-pointer absolute bottom-[10px] right-[50px] transform translate-x-1/2 translate-y-1/2 hover:scale-110 transition-transform duration-300"
+                    onClick={handleCameraClick}
+                />
+                <Input
+                    type="file"
+                    className="hidden"
+                    onChange={handleImageChange}
+                    ref={fileInputRef}
+                />
             </div>
             <div className="flex flex-col gap-[10px]">
                 <Input type="text" placeholder="Título" />
