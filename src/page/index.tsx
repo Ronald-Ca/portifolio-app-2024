7951eec46b0295277648440223367bc74ceda4ae
Ronald Camargo <ronaldcamargodev@gmail.com>
2024-07-14 21:47:40 -0400

    iniciando organização do html

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import Body from "./home-page/body";
 import Footer from "./home-page/footer";
 import Header from "./home-page/header";
+import Projects from "./projects/projects";
+import Skills from "./skill/skills";
+import Contact from "./contact/contact";
+import About from "./about/about";
 
 export default function HomePage() {
+    const [section, setSection] = useState('body')
+
+    const renderSection = () => {
+        switch (section) {
+            case 'projects':
+                return <Projects />
+            case 'skills':
+                return <Skills />
+            case 'contact':
+                return <Contact />
+            case 'about':
+                return <About />
+            default:
+                return <Body />
+        }
+    }
+
     return (
         <div className="h-screen flex flex-col">
-            <Header />
-            <Body />
+            <Header setSection={setSection} />
+            <div className="flex-grow">{renderSection()}</div>
             <Footer />
         </div>
     )
