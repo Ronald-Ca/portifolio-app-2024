0b3661f277cc58347e78e8245f5689169855e77c
Ronald Camargo <ronaldcamargodev@gmail.com>
2024-12-18 20:29:57 -0400

    att

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import * as path from 'path'
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
+	plugins: [react(), tsconfigPaths()],
+	publicDir: 'public',
+	build: {
+		outDir: './build',
+		chunkSizeWarningLimit: 1500,
+	},
+	server: {
+		host: true,
 	},
 })
